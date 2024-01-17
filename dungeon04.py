9ec37f13a7d3a2a74c42496a47c689f0671f901f
Lithe Jniad <99078068@mydavinci.nl>
2024-01-17 22:46:44 +0100

    opdracht04

--- dungeon04.py
+++ dungeon04.py
@@ -4,6 +4,7 @@ import random
 player_attack = 1
 player_defense = 0
 player_health = 3
+rupee = 0
 
 # === [kamer 1] === #
 print('Door de twee grote deuren loop je een gang binnen.')
@@ -12,17 +13,20 @@ print('Je ziet een deur voor je.')
 print('')
 time.sleep(1)
 
+# === [kamer 7] === #
+rupee =+ 1
+print(f"Je krijgt nu {rupee} rupee zodat je straks je eigen wapens kan kopen.")
+print('')
+time.sleep(1)
+
 # === [kamer 2] === #
 schatkist = 'sleutel'
-
 eerste_getal = random.randint(10, 25)
 tweede_getal = random.randint(-5, 75)
 juiste_antwoord = eerste_getal + tweede_getal
 
 print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
 print('Het standbeeld heeft een sleutel vast.')
-print('Op zijn borst zit een numpad met de toetsen 9 t/m 0.')
-
 print(f'Daarboven zie je een som staan {eerste_getal} + {tweede_getal}')
 antwoord = int(input('Wat toest je in? '))
 
@@ -32,28 +36,57 @@ if antwoord == juiste_antwoord:
 else:
     print("helaas dat is niet de juiste antwoord")
 print('Je zie een deur achter het standbeeld.')
-print('')
-time.sleep(1) 
+
+kamers_keuze = input("Je mag tussen twee kamers gaan keizen antwoord met kamer (3/6)? ")
+if kamers_keuze == '6':
+    # === [kamer 6] === # 
+    zombie_attack = 1
+    zombie_defense = 0
+    zombie_health = 2
+
+    print('Je loopt tegen een zombie aan.')
+    zombie_hit_damage = (zombie_attack - player_defense)
+    if zombie_hit_damage <= 0:
+        print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
+    else:
+        zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)   
+        player_hit_damage = (player_attack - zombie_defense)
+        player_attack_amount = math.ceil(zombie_health / player_hit_damage)
+        player_health -= player_attack_amount * zombie_hit_damage
+
+        if player_attack_amount < zombie_attack_amount:
+            print(f'In {player_attack_amount} rondes versla je de zombie.')
+            print(f'Je health is nu {player_health}.')
+        else:
+            print('Helaas is de zombie te sterk voor je.')
+    print('')
+    time.sleep(1)
 
 # === [kamer 3] === #
-item = random.choice(["schild", "Zwaard"])
 
 print('Je duwt hem open en stap een hele lange kamer binnen.')
-if item == "schild":
-    print(f"Je hebt {item} gerkregen en je hebt 1 punt extra voor je defense")
+wapens_keuze = input("Je kan nu je eigen wapen keizen antwoord met (schild/zwaard) anders zeg (niks): ")
+if wapens_keuze == "schild":
     player_defense += 1
-else:
-    print(f"Je hebt {item} gekregen en je hebt 2 punten extra voor je attack ")
+    rupee -= 1
+    print(f"Je hebt een Schild gekocht! en je hebt {rupee} rupee over")
+elif wapens_keuze == "zwaard":
     player_attack += 2
+    rupee -= 1
+    print(f"Je hebt een Zwaard gekocht! en je hebt {rupee} rupee over")
+else:
+    print("Je hebt niks gekocht dat is gevaarlijke voor je tweede vechtpartij")
+
 print('Op naar de volgende deur.')
 print('')
 time.sleep(1)
 
-# === [kamer 4] === #
-zombie_attack = 1
+# === [kamer 4] === # 
+zombie_attack = 2
 zombie_defense = 0
-zombie_health = 2
-print(f'Dapper met je nieuwe {item} loop je de kamer binnen.')
+zombie_health = 3
+
+print(f'Dapper met je nieuwe {wapens_keuze} loop je de kamer binnen.')
 print('Je loopt tegen een zombie aan.')
 
 zombie_hit_damage = (zombie_attack - player_defense)
@@ -61,10 +94,8 @@ if zombie_hit_damage <= 0:
     print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
 else:
     zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
-    
     player_hit_damage = (player_attack - zombie_defense)
     player_attack_amount = math.ceil(zombie_health / player_hit_damage)
-    player_health -= player_attack_amount * zombie_hit_damage
 
     if player_attack_amount < zombie_attack_amount:
         print(f'In {player_attack_amount} rondes versla je de zombie.')
@@ -80,6 +111,7 @@ time.sleep(1)
 print('Voorzichtig open je de deur, je wilt niet nog een zombie tegenkomen.')
 print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
 print('Je loopt er naartoe.')
+
 if antwoord == juiste_antwoord:
     print("Je kan de schatkist open maken je hebt gewonnen")
 else:
